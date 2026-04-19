8b976c887e9ffdc951cf07076b3c251339a3e09a
corsix <corso.davide05@gmail.com>
2026-04-19 23:00:03 +0200

    corretto errore di posizione del file

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
+const User = require('c:/Users/User/Desktop/PROGETTO WEB E MOBILE/FASTFOOD WEBSITE/models/user');
 
 router.post('/register', async (req, res) => {
     try {
