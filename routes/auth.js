34cf7051b8552aeefb8f046ce8f6e6283f11a152
corsixx <corso.davide05@gmail.com>
2026-04-29 15:36:35 +0200

    vari fixes

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('c:/Users/User/Desktop/PROGETTO WEB E MOBILE/FASTFOOD WEBSITE/models/user');
+const User = require('../models/User');
 
 router.post('/register', async (req, res) => {
     try {
