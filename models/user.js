074e3e0119235effcccee6f67e69a6026a3bfe77
corsix <corso.davide05@gmail.com>
2026-08-19 19:34:29 +0200

    update modello user per integrare il profilo del ristoratore e i metodi di pagamento

--- models/user.js
+++ models/user.js
@@ -10,9 +10,32 @@ const userSchema = new mongoose.Schema({
     password: { type: String, required: true }, // Password obbligatoria (verrà salvata già cifrata)
     
     // Ruolo utente: accetta solo 'customer' o 'restaurant', di base è 'customer'
-    role: { type: String, enum: ['customer', 'restaurant'], default: 'customer' }
+    role: { 
+        type: String, 
+        enum: ['customer', 'restaurant'], 
+        default: 'customer' 
+    },
 
-}, { 
+    //dati specifici per il ristoratore (quindi opzionali anche per i clienti)
+    restaurantName: { type: String }, // Nome del ristorante 
+
+    restaurantAddress: { type: String }, // Indirizzo del ristorante 
+
+    restaurantPhone: { type: String }, // Numero di telefono del ristorante 
+
+    IVAnumber: { type: String }, // Partita IVA del ristorante
+
+    //menù del risorante (array di riferimenti ai piatti)
+    restaurantMenu: [{ 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Meal' 
+    }],
+
+    //dati specifici per il cliente
+    paymentMethods: { type: String , default: 'carta' }, // Metodi di pagamento preferiti del cliente
+
+    customerPreferences: [{ type: String }], // Preferenze del cliente
+}, {
     timestamps: true // Aggiunge automaticamente i campi createdAt e updatedAt
 });
 
